ae3d3324bd51e50aaceeca183c4d4be0c642515f
kvishweshwar <dr.kvishwanathan@gmail.com>
2024-01-23 20:36:21 +0530

    updating terraform files..

---
+++ 2ndProject-SampleJavaApplicationDeployment/README.md
@@ -0,0 +1,2 @@
+# jenkinsWithAnsibleTerraform
+Jenkins with ansible and terraform to automate deployment pipelines.
---
+++ 2ndProject-SampleJavaApplicationDeployment/Terraform/networking.tf
@@ -0,0 +1,66 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "ResourceMonitoringVNet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "ResourceMonitoringSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "ResourceMonitoringNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "IPAddressAllocation"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+
+resource "azurerm_public_ip" "pip" {
+  name                = "ResourceMonitoringPublicIP"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+}
+
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "ResourceMonitoringNSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "rule" {
+  for_each = {
+    ssh        = { priority = 1001, port = 22 }
+    grafana    = { priority = 1002, port = 3000 }
+    prometheus = { priority = 1003, port = 9090 }
+  }
+
+  name                        = "${upper(each.key)}InboundRule"
+  priority                    = each.value.priority
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value.port
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
